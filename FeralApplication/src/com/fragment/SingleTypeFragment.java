39f1dd745cefdafd3ffde08c21992f81cfd4f2e9
nuboo <1134533177@qq.com>
2016-03-03 21:04:07 +0800

    第一次提交

---
+++ FeralApplication/src/com/fragment/SingleTypeFragment.java
@@ -0,0 +1,29 @@
+﻿package com.fragment;
+
+
+
+import com.yooikee.adapter.HorizontalListView;
+import com.yooikee.adapter.SingleTypeAdapter;
+import com.yooikee.feralapplication.R;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class SingleTypeFragment extends Fragment {
+	String[] str={"1","2","3","4"};
+	
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		View view =inflater.inflate(R.layout.single_type, null);
+		HorizontalListView listView = (HorizontalListView) view.findViewById(R.id.horizontal_list_view);
+		SingleTypeAdapter adapter=new SingleTypeAdapter(getActivity().getApplicationContext(), str);
+		listView.setAdapter(adapter);
+		return view;
+	}
+}
---
+++ FeralApplication/src/com/yooikee/data/CourseClassInfo.java
@@ -0,0 +1,54 @@
+﻿package com.yooikee.data;
+
+public class CourseClassInfo {
+      public int getClassIcon() {
+		return classIcon;
+	}
+	public void setClassIcon(int classIcon) {
+		this.classIcon = classIcon;
+	}
+	public String getClassTitle() {
+		return classTitle;
+	}
+	public void setClassTitle(String classTitle) {
+		this.classTitle = classTitle;
+	}
+	public String getClassPeople() {
+		return classPeople;
+	}
+	public void setClassPeople(String classPeople) {
+		this.classPeople = classPeople;
+	}
+	public String getClassSource() {
+		return classSource;
+	}
+	public void setClassSource(String classSource) {
+		this.classSource = classSource;
+	}
+	public String getClassArticle() {
+		return classArticle;
+	}
+	public void setClassArticle(String classArticle) {
+		this.classArticle = classArticle;
+	}
+	public String getClassGrade() {
+		return classGrade;
+	}
+	public void setClassGrade(String classGrade) {
+		this.classGrade = classGrade;
+	}
+	public String getClassEndTime() {
+		return classEndTime;
+	}
+	public void setClassEndTime(String classEndTime) {
+		this.classEndTime = classEndTime;
+	}
+	int classIcon;//图标
+      String classTitle;//课程标题
+      String classPeople;//课程报名人数
+      String classSource;//课程培训方
+      String classArticle;//课程方向
+      String classGrade;//等级
+      String classEndTime;//报名截止剩余时间
+      
+}
