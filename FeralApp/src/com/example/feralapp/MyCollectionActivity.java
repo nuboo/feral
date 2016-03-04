072dc78ef33308577d599c97399732b3b904ba80
nuboo <1134533177@qq.com>
2016-03-04 18:08:46 +0800

    微调

--- FeralApp/src/com/example/feralapp/MyCollectionActivity.java
+++ FeralApp/src/com/example/feralapp/MyCollectionActivity.java
@@ -1,5 +1,24 @@
 package com.example.feralapp;
 
-public class MyCollectionActivity {
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ListView;
 
+/**
+ * 我的收藏
+ * 
+ * @author admin
+ *
+ */
+public class MyCollectionActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_my_collection);
+	}
+
+	public void innt() {
+		ListView listView = (ListView) findViewById(R.id.list_view);
+	}
 }
--- FeralApp/src/com/example/model/MyCollection.java
+++ FeralApp/src/com/example/model/MyCollection.java
@@ -1,5 +1,11 @@
 package com.example.model;
 
+/**
+ * 我的收藏实体类
+ * 
+ * @author admin
+ *
+ */
 public class MyCollection {
 
 }
